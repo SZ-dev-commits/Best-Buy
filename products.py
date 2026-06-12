2712bf09f137acf22bd1841484482c04c2112367
Shunda <zhang.shunda@hotmail.com>
2026-06-12 11:49:41 +0200

    add new product types and tests

--- products.py
+++ products.py
@@ -41,3 +41,30 @@ class Product:
             self.active = False
 
         return total_price
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, quantity=0)
+
+    def set_quantity(self, quantity):
+        pass
+
+    def buy(self, quantity):
+        return self.price * quantity
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: Unlimited"
+
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self.maximum = maximum
+
+    def buy(self, quantity):
+        if quantity > self.maximum:
+            raise Exception(f"Cannot buy more than {self.maximum} items of this product at once!")
+        return super().buy(quantity)
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}, Maximum per order: {self.maximum}"
